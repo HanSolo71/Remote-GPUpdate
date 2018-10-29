823ea248e71b61c3495c05d941fa2ac0d6c3f0dc
Austin Downing <HanSolo552@gmail.com>
2018-10-29 12:00:51 -0400

    Add files via upload

---
+++ Remote GPUpdate.ps1
@@ -0,0 +1,11 @@
+﻿#Gets all workstations that need to have software installed, if you don't want to uninstall all of the software from you will need to use a text document and Get-Content
+$computers = Get-ADComputer -Filter * -SearchBase "DC=Contoso,DC=COM" | Select DNSHostName -ExpandProperty DNSHostname
+
+#Gets computers to run on from text file
+#$computers = Get-Content '\\FileShare\Computers.txt'
+
+foreach ($Computer in $Computers) 
+{
+$computer
+Invoke-GPUpdate -Computer $Computer -RandomDelayInMinutes 0 -Sync
+}
